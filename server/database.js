4615bbafb011eaeac3b867017a0e46d0a952f8eb
Muhammad Firas <112752858+firas-07@users.noreply.github.com>
2024-10-20 20:04:52 +0530

    Add files via upload

---
+++ server/database.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/',(req,res)=>{
+    res.send("Server is Running Successfully...")
+})
+mongoose.connect('mongodb://localhost:27017/portfolio-contact', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+// Information Schema
+const InformationSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    phone: { type: String, required: true },
+    message: { type: String, required: true },
+});
+
+const Information = mongoose.model('Information', InformationSchema);
+
+// API endpoint to handle form submissions
+app.post('/submit-form', async (req, res) => {
+    const { name, email, phone, message } = req.body;
+
+    try {
+        const info = new Information({
+            name,
+            email,
+            phone,
+            message
+        });
+
+        await info.save();
+        res.status(200).json({ message: 'Form submitted successfully!' });
+    } catch (error) {
+        res.status(500).json({ error: 'Error saving form data' });
+    }
+});
+
+// Start the server
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
