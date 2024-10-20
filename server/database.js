421ce363892e9833cce57b370f69deff0256decf
Muhammad Firas <112752858+firas-07@users.noreply.github.com>
2024-10-20 12:45:46 +0530

    Update database.js

--- server/database.js
+++ server/database.js
@@ -3,7 +3,13 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
-app.use(cors());
+app.use(cors(
+    {
+        origin: ["https://firas-portfolio-chi.vercel.app/"],
+        methods: ["POST", "GET"],
+        crendtials: true
+    }
+));
 app.use(bodyParser.json());
 
 mongoose.connect('mongodb://localhost:27017/portfolio-contact', {
